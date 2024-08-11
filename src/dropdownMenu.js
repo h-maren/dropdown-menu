db0956b376e95f12e2b783ff6c2f23b5b559ab19
h-maren <26232423+h-maren@users.noreply.github.com>
2024-08-11 12:27:11 -0400

    Create drop down menu

---
+++ src/dropdownMenu.js
@@ -0,0 +1,11 @@
+import { navLinks } from '.'
+
+let showMenu = (function (e) {
+    navLinks.classList.remove("hidden");
+});
+
+let hideMenu = (function (e) {
+    navLinks.classList.add("hidden");
+});
+
+export { showMenu, hideMenu }
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+import  './menu.svg';
+import { showMenu, hideMenu } from './dropdownMenu.js'
+
+
+const navLinks=document.querySelector(".nav-links");
+navLinks.addEventListener("mouseleave",hideMenu)
+navLinks.addEventListener("mouseover",showMenu);
+
+const navMenuBtn=document.querySelector(".nav-icon");
+navMenuBtn.addEventListener("mouseover",showMenu);
+navMenuBtn.addEventListener("mouseleave",hideMenu);
+
+export { navLinks }
